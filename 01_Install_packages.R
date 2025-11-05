e0cf244a7a46830b2e1f0f44a8a2b18ec3cece56
ThijsRU <46558447+ThijsRU@users.noreply.github.com>
2025-11-05 12:30:42 +0100

    Delete 01_Install_packages.R

--- 01_Install_packages.R
+++
@@ -1,9 +0,0 @@
-# Step 3. Install dplyr, tidyr and data.table packages
-
-# Because the MEET uses the dplyr, tidyr and data.table R-packages, it is necessary to install 
-# and load these packages. The packages have to be loaded using "library" each time 
-# you open the project. This can be done with each of the scripts that are part of the MET.
-
-install.packages("dplyr")
-install.packages("tidyr")
-install.packages("data.table")
